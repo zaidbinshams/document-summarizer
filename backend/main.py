ea5f1b95320b5f35bd0b152ad1a9fc40dc0b510f
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-08-25 00:48:47 +0530

    Configure frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,10 @@ ALLOWED_TYPES = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
