f11b79832f8108b317d123bfa7207b06d57128e3
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-08-25 01:07:55 +0530

    Allow production frontend origin

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://document-summarizer.pages.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
